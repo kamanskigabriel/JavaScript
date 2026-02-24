0b310e07eee6d02e67746856af7d23195716c4b0
gabriel kamanski <gabriel62318326@alunos.sc.senac.br>
2026-02-23 21:27:34 -0300

    script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JavaScript
 Repositorio de JavaScript (BOM) Java (Uma bosta)
+
+Introdução ao JS pro,pts e alerts
---
+++ script/script.js
@@ -0,0 +1,8 @@
+//Não usar espaço, ACENTO, Ç, @¨&#*@*
+
+// Number = função que tenta converter um valor passado para numero
+
+const numero = Number(prompt("coloque um número"))
+const outronumero = Number(prompt("coloque outro número"))
+const maisnumero = Number(prompt("coloque outro número"))
+alert(numero+outronumero+maisnumero)
